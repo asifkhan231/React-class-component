3e7f314e347c6372c05d682c9780ccfdaf2a586e
Asif <ak3226519@gmail>
2024-08-17 01:51:21 +0530

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
-import EventHandler from './component/eventHandler';
-import EventBinding from './component/eventBinding';
+import EventBinding from './component/eventHandler/eventBinding';
+import EventHandler from './component/eventHandler/eventHandler';
+import Hero from './component/errorBoundary/hero';
+import ErrorBoundary from './component/errorBoundary/errorBoundary';
 
- class App extends Component {
+
+
+class App extends Component {
   constructor() {
     super();
-    
-    }
-  
+
+  }
+
 
   render() {
     return (
       <div>
         {/* <EventHandler/> */}
-        <EventBinding/>
+        <EventBinding />
+
+
+        <ErrorBoundary>
+          <Hero name="Suparman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero name="Batman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero name="joker" />
+        </ErrorBoundary>
       </div>
     )
   }
